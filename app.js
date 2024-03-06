98d38158999222e98e3c321049978e4b0a4a0438
gurutej7 <ngurutej47@gmail.com>
2024-03-06 22:22:17 +0530

    create custom api errors

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ require("dotenv").config();
 require("express-async-errors");
 
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
 const app = express();
 
 // connectDB
 const connectDB = require("./db/connect");
+
+// middlewares
+app.use(cors());
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true,limit:"16kb"}));
+app.use(express.static("public"));
+app.use(cookieParser());
+
 
 const port = process.env.PORT || 5000;
 
